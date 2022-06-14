8598860e31510f35ffd7e24d193dff2ab638ecf9
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-06-14 17:06:03 -0400

    Reorg and consolidate codebase, move completion logic from copilot.lua into copilot_cmp

--- lua/copilot_cmp/source.lua
+++ lua/copilot_cmp/source.lua
@@ -1,15 +1,4 @@
 local source = {}
-local existing_matches = {}
-local util = require("copilot.util")
-local formatter = require("copilot_cmp.format")
-
-source.new = function(client)
-  local self = setmetatable({ timer = vim.loop.new_timer() }, { __index = source })
-  self.client = client
-  self.request_ids = {}
-  return self
-end
-
 source.get_trigger_characters = function()
   return { "\t", "\n", ".", ":", "(", "'", '"', "[", ",", "#", "*", "@", "|", "=", "-", "{", "/", "\\", " ", "+", "?"}
 end
@@ -29,21 +18,13 @@ source.is_available = function(self)
   return true
 end
 
-source.complete = function(_, params, callback)
-  existing_matches[params.context.bufnr] = existing_matches[params.context.bufnr] or {}
-  existing_matches[params.context.bufnr][params.context.cursor.row] = existing_matches[params.context.bufnr][params.context.cursor.row] or { IsIncomplete = true }
-  local existing = existing_matches[params.context.bufnr][params.context.cursor.row]
-  local has_complete = false
-  vim.lsp.buf_request(0, "getCompletionsCycling", util.get_completion_params(), function(_, response)
-    if response and not vim.tbl_isempty(response.completions) then
-      existing = vim.tbl_deep_extend("force", existing, formatter.format_completions(params, response.completions))
-      has_complete = true
-    end
-    vim.schedule(function() callback(existing) end)
-  end)
-  if not has_complete then
-    callback(existing)
-  end
+source.new = function(client, completion_fn)
+  local completion_functions = require("copilot_cmp.completion_functions")
+  local self = setmetatable({ timer = vim.loop.new_timer() }, { __index = source })
+  self.client = client
+  self.request_ids = {}
+  self.complete = completion_fn and completion_functions.init(completion_fn) or completion_functions.init("getCompletionsCycling")
+  return self
 end
 
 return source
