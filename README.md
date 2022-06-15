3c2b09624d8babe941831e5ea421970e38916a9e
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-06-15 17:24:41 -0400

    Huge performance improvements and cleanup

--- README.md
+++ README.md
@@ -23,6 +23,8 @@ If you do not have copilot.lua installed, go to https://github.com/zbirenbaum/co
 
 ### Configuration
 
+##### Source Definition
+
 To link cmp with this source, simply go into your cmp configuration file and include `{ name = "copilot" }` under your sources
 
 Here is an example of what it should look like:
@@ -31,10 +33,69 @@ Here is an example of what it should look like:
 cmp.setup {
   ...
   sources = {
+    -- Copilot Source
     { name = "copilot", group_index = 2 },
+    -- Other Sources
     { name = "nvim_lsp", group_index = 2 },
     { name = "path", group_index = 2 },
     { name = "luasnip", group_index = 2 },
+  },
+  ...
+}
+```
+
+##### Highlighting
+
+You can create a custom entry label and highlight group for copilot completions similar to those for different Lsp kinds by modifying the format function of your cmp config. By default, copilot entries will appear with the `Snippet` label and highlight
+
+Example:
+
+```lua
+cmp.setup {
+  ...
+  formatting = {
+    format = function (entry, vim_item)
+      if entry.source.name == "copilot" then
+        vim_item.kind = "[] Copilot"
+        vim_item.kind_hl_group = "CmpItemKindCopilot"
+        return vim_item
+      end
+      return lspkind.cmp_format({ with_text = false, maxwidth = 50 })(entry, vim_item)
+    end
+  }
+  ...
+}
+
+vim.api.nvim_set_hl(0, "CmpItemKindCopilot", {fg ="#6CC644"})
+```
+
+##### Comparators
+
+Two customs comparitors for sorting cmp entries are provided: `score` and `prioritize`. The `prioritize` comparitor causes copilot entries to appearhigher in the cmp menu. The `score` comparitor only does something if getPanelCompletions is the method used in the cmp field of the copilot.lua config. It is recommended keeping priority weight at 2, or placing the `exact` comparitor above copilot so that better lsp matches are not stuck below poor copilot matches.
+
+Example:
+
+```lua
+cmp.setup {
+  ...
+  sorting = {
+    priority_weight = 2,
+    comparators = {
+      require("copilot_cmp.comparators").prioritize,
+      require("copilot_cmp.comparators").score,
+
+      -- Below is the default comparitor list and order for nvim-cmp
+      cmp.config.compare.offset,
+      -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      cmp.config.compare.recently_used,
+      cmp.config.compare.locality,
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
   },
   ...
 }
--- lua/copilot_cmp/completion_functions.lua
+++ lua/copilot_cmp/completion_functions.lua
@@ -1,88 +1,84 @@
 local formatter = require("copilot_cmp.format")
 local util = require("copilot.util")
 local handler = require("copilot.handlers")
-local panel = require("copilot.extensions.panel")
 local methods = { id = 0 }
 
--- 1. setup handlers to recieve notifications
--- 2. recieve cmp callback and info
--- 3. ???
--- 4. send request to copilot
-methods.verify = function (bufnr, row)
-  methods.existing_matches[bufnr] = methods.existing_matches[bufnr] or {}
-  methods.existing_matches[bufnr][row] = methods.existing_matches[bufnr][row] or {}
-end
-
--- add single
-methods.add_result = function (completion, bufnr, row)
-  methods.verify(bufnr, row)
-  methods.existing_matches[bufnr][row][completion.label] = completion
+-- TODO: Clean up cycling a bit
+-- Compared to PanelCompletions, cycling isn't great
+-- All these local methods just used by cycling is a pretty huge waste
+local add_result = function (completion, params)
+  if not completion then return end
+  local bufnr = params.context.bufnr
+  local row = params.context.cursor.row
+  local existing_matches = methods.existing_matches
+  existing_matches[bufnr][row][formatter.deindent(completion.text)] = completion
+  return existing_matches[bufnr][row]
 end
 
 -- add multiple
-methods.add_results = function (completions, bufnr, row)
-  for _, completion in ipairs(completions) do
-    methods.add_result(completion, bufnr, row)
-  end
-end
-
-methods.get_completions = function (bufnr, row)
-  return vim.tbl_values(methods.existing_matches[bufnr][row] or {})
-end
-
-methods.init_handlers = function ()
-  local completed = function ()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local row = vim.api.nvim_win_get_cursor(0)[1]
-    local callback = methods.get_cmp_callback()
-    callback({ isIncomplete = false, items = methods.get_completions(bufnr, row) })
-  end
-
-  -- this callback is local and independent of params and the cmp callback
-  -- it must be this way in case an old completion request comes in
-  -- in this scenario, the old request is still added to the persistent completions cache
-  local add_completion_callback = function (completion)
-    completion.text = completion.displayText
-    local end_range = completion.range["end"]
-    local bufnr = tonumber(completion.panelId)
-    local row = end_range.line+1
-    local cursor = { line = row-1, row = row, character = end_range.character, col = end_range.character+1 }
-    local solution = formatter.format_item(completion, cursor)
-    methods.add_result(solution, bufnr, row)
-  end
-
-  handler.add_handler_callback("PanelSolution", "cmp", add_completion_callback)
-
-  handler.add_handler_callback("PanelSolutionsDone", "cmp", completed)
+local add_results = function (completions, params)
+  local existing_matches_loc = {}
+  for _, completion in ipairs(completions) do existing_matches_loc = add_result(completion, params) end
+  return existing_matches_loc
 end
 
 methods.getCompletionsCycling = function (_, params, callback)
   local bufnr = params.context.bufnr
   local row = params.context.cursor.row
-  methods.verify(bufnr, row)
+  methods.existing_matches[bufnr] = methods.existing_matches[bufnr] or {}
+  methods.existing_matches[bufnr][row] = methods.existing_matches[bufnr][row] or {}
+
   vim.lsp.buf_request(0, "getCompletionsCycling", util.get_completion_params(), function(_, response)
-    if not response or vim.tbl_isempty(response.completions) then
-      callback({ IsIncomplete=true, items = methods.get_completions(bufnr, row) })
-      return
-    end
-    local completions = formatter.format_completions(response.completions, params.context.cursor)
-    methods.add_results(completions, bufnr, row)
-    callback({ IsIncomplete=false, items = methods.get_completions(bufnr, row) })
+    if not response or vim.tbl_isempty(response.completions) then return end --j
+    methods.existing_matches[bufnr][row] = add_results(response.completions, params)
+    local existing_matches = methods.existing_matches[bufnr][row]
+    local completions = formatter.format_completions(vim.tbl_values(existing_matches or {}), params)
+    callback({ IsIncomplete=true, items = completions })
   end)
-  callback({ IsIncomplete=true, items = methods.get_completions(bufnr, row) })
+  local completions = formatter.format_completions(vim.tbl_values(methods.existing_matches[bufnr][row] or {}), params)
+  callback({ IsIncomplete=true, items = completions })
 end
 
+
+--[[
+In the entirity of copilot.lua and copilot_cmp this is probably the most complex, but also elegant code
+
+Every other way of doing this was slow, awful, or gave way too many suggestions from cache rather than new results
+
+Somehow, though, this works way better than cycling ever did
+
+getPanelCompletions's handler in copilot lsp returns in 3 parts:
+  1. First, the actual immediately getPanel Completionsresponse is just how many you get max. this is useless and inaccurate information. The handler in the copilot lsp then triggers two notifications to dispatch
+  2. It sends `PanelSolution` notification a # of times equal to however many completions it said it would in (1).
+  3. It sends `PanelCompletionsDone` notification once after all PanelSolutions are sent.
+
+Here's the breakdown on how I handle it:
+  1. make a variable for ID that will keep track of calls to the function (in init)
+  2. use the id to create_handlers pre-emptively with the current call's callbacks and params as variables in their scope every time getPanelCompletions is called
+  3. the panelId field is returned along with the results of getPanelSolutions, then updating vim.lsp.handlers[handler].
+  4. The calls to `handler` made here add callbacks to the config of the corresponding notification handler by adding them to a the module's callback table
+  5. Each of the callbacks we add to PanelSolutions just inserts the solution into a table local to the create_handlers scope it was created in. If another one request hasbeen dispatched, it won't interfere
+  6. PanelSolutionsDone's handler makes use of the id function to get the specific cmp callback and params that corresponds to the solutions that are now present in the results table. It then formats these completions and sends them to cmp
+  7. Finally, clean up the hooks so that we don't eat up memory
+--]]
 
 local create_handlers = function (id, params, callback)
   local results = {}
   id = tostring(id)
-
-  handler.add_id_callback("PanelSolution", id, function (solution)
-    table.insert(results, solution)
+  handler.add_handler_callback("PanelSolution", id, function (solution)
+    -- this standardizes the format of the response to be the same as cycling
+    -- Cycling insertions have been empirically less buggy
+    solution.range.start = {
+      character = 0,
+      line = solution.range.start.line,
+    }
+    solution.text = solution.displayText
+    solution.displayText = solution.completionText
+    results[formatter.deindent(solution.text)] = solution --ensure unique
   end)
 
   handler.add_handler_callback("PanelSolutionsDone", id, function()
-    callback({ IsIncomplete = false, items = formatter.format_completions(results, params) })
+    callback({ IsIncomplete = true, items = formatter.format_completions(vim.tbl_values(results), params) })
     vim.schedule(function () handler.remove_all_name(id) end)
   end)
 end
@@ -101,7 +97,9 @@ methods.getPanelCompletions = function (self, params, callback)
     if err then return end
     create_handlers(id, params, callback)
   end
-  request("getPanelCompletions", req_params(id), respond_callback)
+  local sent, _ = request("getPanelCompletions", req_params(id), respond_callback)
+  if not sent then handler.remove_all_name(id) end
+  callback({ IsIncomplete = true, items = {}})
 end
 
 methods.init = function (completion_method)
