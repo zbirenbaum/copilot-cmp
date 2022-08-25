bc6cb94373a24d17a14acdb04210fb4dc12d0e76
J S <49557684+svilupp@users.noreply.github.com>
2022-08-25 21:34:38 +0100

    First draft
    Misses copilot consumption logic

--- lua/copilot_cmp/completion_functions.lua
+++ lua/copilot_cmp/completion_functions.lua
@@ -3,41 +3,44 @@ local util = require("copilot.util")
 local handler = require("copilot.handlers")
 local methods = { id = 0 }
 
--- TODO: Clean up cycling a bit
--- Compared to PanelCompletions, cycling isn't great
--- All these local methods just used by cycling is a pretty huge waste
-local add_result = function (completion, params)
-  if not completion then return end
-  local bufnr = params.context.bufnr
-  local row = params.context.cursor.row
-  local existing_matches = methods.existing_matches
-  existing_matches[bufnr][row][formatter.deindent(completion.text)] = completion
-  return existing_matches[bufnr][row]
-end
-
--- add multiple
-local add_results = function (completions, params)
-  local existing_matches_loc = {}
-  for _, completion in ipairs(completions) do existing_matches_loc = add_result(completion, params) end
-  return existing_matches_loc
-end
-
-methods.getCompletionsCycling = function (_, params, callback)
-  local bufnr = params.context.bufnr
-  local row = params.context.cursor.row
-  methods.existing_matches[bufnr] = methods.existing_matches[bufnr] or {}
-  methods.existing_matches[bufnr][row] = methods.existing_matches[bufnr][row] or {}
-  vim.lsp.buf_request(0, "getCompletionsCycling", util.get_completion_params(), function(_, response)
-    if not response or vim.tbl_isempty(response.completions) then return end --j
-    methods.existing_matches[bufnr][row] = add_results(response.completions, params)
-    local existing_matches = methods.existing_matches[bufnr][row]
-    local completions = formatter.format_completions(vim.tbl_values(existing_matches or {}), params)
-    callback(completions)
+-- QQ: This seems to need a different name but naming convention is not clear
+local create_handlers_cycling = function (id, params, callback)
+  local results = {}
+  id = tostring(id)
+  -- QQ: how are solutions provided to this handler
+  handler.add_handler_callback("getCompletionsCycling", id, function (solution)
+    solution.range.start = {
+      character = 0,
+      line = solution.range.start.line,
+    }
+    solution.text = solution.displayText
+    solution.displayText = solution.completionText
+    results[formatter.deindent(solution.text)] = solution --ensure unique
+    callback({
+      IsIncomplete = true,
+      items = formatter.format_item(solution, params)
+    })
   end)
-  local completions = formatter.format_completions(vim.tbl_values(methods.existing_matches[bufnr][row] or {}), params)
-  callback(completions)
+
+  -- TODO: Is this needed?
+  -- handler.add_handler_callback("PanelSolutionsDone", id, function()
+  --   callback(formatter.format_completions(vim.tbl_values(results), params))
+  --   vim.schedule(function () handler.remove_all_name(id) end)
+  -- end)
 end
 
+methods.getCompletionsCycling = function (self, params, callback)
+  local request = self.client.rpc.request
+  local id = methods.id
+  local respond_callback = function (err, _)
+    methods.id = methods.id + 1
+    if err then return end
+    create_handlers_cycling(id, params, callback)
+  end
+  local sent, _ = request("getCompletionsCycling", req_params(id), respond_callback)
+  if not sent then handler.remove_all_name(id) end
+  callback({ IsIncomplete = true, items = {}})
+end
 
 --[[
 In the entirity of copilot.lua and copilot_cmp this is probably the most complex, but also elegant code
